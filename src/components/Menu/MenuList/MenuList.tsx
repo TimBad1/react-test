eb413a88b7ff749d509b445d42a013c99cf9854c
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-10-27 09:02:38 +0300

    fix MenuList

--- src/components/Menu/MenuList/MenuList.tsx
+++ src/components/Menu/MenuList/MenuList.tsx
@@ -2,31 +2,33 @@ import React from 'react';
 import { MenuItem } from '../MenuItem';
 import './menulist.sass';
 
+interface IMenuItem {
+  icon: React.ReactElement;
+  id: number;
+  name: string;
+  menuList?: any;
+}
+
 interface IMenuList {
-  menuList: {
-    icon: React.ReactElement;
-    id: number;
-    name: string;
-    menuList?: any;
-  }[]
+  menuList: IMenuItem[]
 }
 
 export function MenuList(menuList: IMenuList) {
   // console.log(menuList);
   return (
     <ul className="menu__list">
-    {menuList.menuList && menuList.menuList.map((item: any) => {
-      console.log(item);
+    {menuList.menuList.map((item: IMenuItem) => {
+      console.log(item.menuList);
       return (<li className="menu__item" key={item.id}>
         <MenuItem 
           name={item.name} 
           icon={item.icon} 
         />
-        
-      </li>)
-    }
-      
-      
+        {item.menuList && 
+        <MenuList menuList={item.menuList}/>
+        }
+      </li>) 
+    }      
     )}
   </ul>
   );
